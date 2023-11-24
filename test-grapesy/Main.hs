6e4dfed99f64802bb7f84b2e19333612f656b39c
Edsko de Vries <edsko@well-typed.com>
2023-11-24 18:05:33 +0100

    Remove redundant code
    Next step: inline runTestClientServer

--- test-grapesy/Main.hs
+++ test-grapesy/Main.hs
@@ -30,12 +30,9 @@ import Control.Tracer
 import Data.Bifunctor
 import Data.Default
 import Data.Proxy
-import Data.Set (Set)
-import GHC.Generics qualified as GHC
 import Network.HTTP2.Internal qualified as HTTP2
 import Network.HTTP2.Server qualified as HTTP2
 import System.Exit
-import Text.Show.Pretty
 
 import Network.GRPC.Client qualified as Client
 import Network.GRPC.Client.Binary qualified as Client.Binary
@@ -228,72 +225,6 @@ runTestClientServer clientRun serverHandlers = do
 
 -- ========================================================================== --
 
-data LocalStep =
-    ClientAction (Action (Metadata, RPC) NoMetadata)
-  | ServerAction (Action Metadata        Metadata)
-  deriving stock (Show, Eq, GHC.Generic)
-  deriving anyclass (PrettyVal)
-
-data Action a b =
-    -- | Initiate request and response
-    --
-    -- When the client initiates a request, they can specify a timeout, initial
-    -- metadata for the request, as well as which endpoint to connect to. This
-    -- must happen before anything else.
-    --
-    -- On the server side an explicit 'Initiate' is not required; if not
-    -- present, there will be an implicit one, with empty metadata, on the first
-    -- 'Send'.
-    Initiate a
-
-    -- | Send a message to the peer
-  | Send (StreamElem b Int)
-
-    -- | Early termination (cleanly or with an exception)
-  | Terminate (Maybe ExceptionId)
-
-    -- | Sleep specified number of milliseconds
-    --
-    -- This is occassionally useful, for example to have the client keep the
-    -- connection open to the server for a bit longer, without actually doing
-    -- anything with that connection.
-  | SleepMilli Int
-  deriving stock (Show, Eq, GHC.Generic)
-  deriving anyclass (PrettyVal)
-
-data RPC = RPC1 | RPC2 | RPC3
-  deriving stock (Show, Eq, GHC.Generic)
-  deriving anyclass (PrettyVal)
-
--- | Metadata
---
--- We use 'Set' for 'CustomMetadata' rather than a list, because we do not
--- want to test that the /order/ of the metadata is matched.
-type Metadata = Set CustomMetadata
-
-{-------------------------------------------------------------------------------
-  User exceptions
-
-  When a test calls for the client or the server to throw an exception, we throw
-  one of these. Their sole purpose is to be "any" kind of exception (not a
-  specific one).
--------------------------------------------------------------------------------}
-
-data SomeServerException = SomeServerException ExceptionId
-  deriving stock (Show, GHC.Generic)
-  deriving anyclass (Exception, PrettyVal)
-
-data SomeClientException = SomeClientException ExceptionId
-  deriving stock (Show, GHC.Generic)
-  deriving anyclass (Exception, PrettyVal)
-
--- | We distinguish exceptions from each other simply by a number
-newtype ExceptionId = ExceptionId Int
-  deriving stock (Show, Eq, GHC.Generic)
-  deriving anyclass (PrettyVal)
-
--- ========================================================================== --
-
 {-------------------------------------------------------------------------------
   Endpoints
 -------------------------------------------------------------------------------}
@@ -318,7 +249,7 @@ clientLocal1 testClock withConn = do
     withConn $ \conn ->
       Client.withRPC conn def (Proxy @TestRpc1) $ \_call -> do
         waitForTestClockTick testClock $ TestClockTick 2
-        throwIO $ SomeClientException (ExceptionId 0)
+        throwIO $ userError "client exception"
 
 clientLocal2 ::
      HasCallStack
