de1fda419c803299191d7c345a09da7225735663
Edsko de Vries <edsko@well-typed.com>
2023-12-07 14:48:22 +0100

    Remove unused modules

--- src/Network/GRPC/Common.hs
+++ src/Network/GRPC/Common.hs
@@ -23,9 +23,6 @@ module Network.GRPC.Common (
   , NoMetadata(..)
   , customHeaderName
 
-    -- * Configuration
-  , SslKeyLog(..)
-
     -- * Exceptions
   , GrpcException(..)
   , GrpcError(..)
@@ -44,7 +41,6 @@ import Network.GRPC.Common.StreamElem (StreamElem(..))
 import Network.GRPC.Spec
 import Network.GRPC.Util.Session qualified as Session
 import Network.GRPC.Util.Thread qualified as Thread
-import Network.GRPC.Util.TLS
 
 {-------------------------------------------------------------------------------
   Exceptions
