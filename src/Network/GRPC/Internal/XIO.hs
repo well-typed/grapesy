17fc8db2091f683d46346b23fb71dd748fd621a5
Edsko de Vries <edsko@well-typed.com>
2024-07-25 11:27:00 +0200

    Remove XIO leftover

--- src/Network/GRPC/Internal/XIO.hs
+++
@@ -1,8 +0,0 @@
--- | Temporary re-export of @Control.Monad.XIO@
---
--- TODO: Eventually @Control.Monad.XIO@ should move to its own package.
-module Network.GRPC.Internal.XIO (
-    module Control.Monad.XIO
-  ) where
-
-import Control.Monad.XIO
