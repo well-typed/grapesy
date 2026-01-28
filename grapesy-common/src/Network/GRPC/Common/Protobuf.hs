6981318d177b94768a1c5ad87433c417a2712601
Oleg Grenrus <oleg.grenrus@iki.fi>
2026-01-28 13:06:04 +0200

    Copy grapesy into two split packages...
    - grapesy-common and
    - grapesy-http2
    
    The grapesy-common doesn't depend on http2
    
    For now, the .Client part has to be copied almost entirely into
    grapesy-http2, as Connection type depends on HTTP2 bits and a lot builds
    on top of that.  I haven't disentangled that yet.

---
+++ grapesy-common/src/Network/GRPC/Common/Protobuf.hs
@@ -0,0 +1,143 @@
+{-# LANGUAGE OverloadedLabels #-}
+
+-- | Common functionality for working with Protobuf
+module Network.GRPC.Common.Protobuf (
+    Protobuf
+  , Proto(..)
+  , getProto
+
+    -- * Exceptions
+  , ProtobufError(..)
+  , throwProtobufError
+  , throwProtobufErrorHom
+  , toProtobufError
+  , toProtobufErrorHom
+
+    -- * Re-exports
+    -- ** "Data.Function"
+  , (&)
+    -- ** "Control.Lens"
+  , (.~)
+  , (^.)
+  , (%~)
+    -- ** "Data.ProtoLens"
+  , StreamingType(..)
+  , HasField(..)
+  , FieldDefault(..)
+  , Message(defMessage)
+  ) where
+
+import Network.GRPC.Util.Imports
+
+import Control.Lens ((.~), (^.), (%~))
+import Control.Monad ((<=<))
+import Control.Monad.Except (throwError)
+import Data.Function ((&))
+import Data.Int (Int32)
+import Data.ProtoLens.Field (HasField(..), field)
+import Data.ProtoLens.Message (FieldDefault(..), Message(defMessage))
+
+import Network.GRPC.Common.Protobuf.Any (Any)
+import Network.GRPC.Common.Protobuf.Any qualified as Any
+
+{-------------------------------------------------------------------------------
+  Protobuf-specific errors
+-------------------------------------------------------------------------------}
+
+-- | gRPC exception with protobuf-specific error details
+--
+-- See also 'Status' and @google.rpc.Status@.
+data ProtobufError a = ProtobufError {
+      protobufErrorCode     :: GrpcError
+    , protobufErrorMessage  :: Maybe Text
+    , protobufErrorDetails  :: [a]
+    }
+  deriving stock (Show, Eq, Ord, Functor, Foldable, Traversable)
+
+-- | Throw 'GrpcException' with Protobuf-specific details
+throwProtobufError :: ProtobufError (Proto Any) -> IO x
+throwProtobufError ProtobufError{
+                       protobufErrorCode
+                     , protobufErrorMessage
+                     , protobufErrorDetails
+                     } = throwIO $ GrpcException {
+      grpcError         = protobufErrorCode
+    , grpcErrorMessage  = protobufErrorMessage
+    , grpcErrorDetails  = Just $ buildStatus status
+    , grpcErrorMetadata = []
+    }
+  where
+    status :: Proto Status
+    status =
+        defMessage
+          & field @"code"    .~ fromIntegral (fromGrpcError protobufErrorCode)
+          & field @"message" .~ fromMaybe fieldDefault protobufErrorMessage
+          & field @"details" .~ protobufErrorDetails
+
+-- | Variation of 'throwProtobufError' for a homogenous list of details
+--
+-- The @google.rpc.Status@ message uses the Protobuf 'Any' type to store a
+-- heterogenous list of details. In case that all elements in this list are
+-- actually of the /same/ type, we can provide a simpler API.
+throwProtobufErrorHom :: Message a => ProtobufError (Proto a) -> IO x
+throwProtobufErrorHom = throwProtobufError . fmap Any.pack
+
+-- | Construct 'ProtobufError' by parsing 'grpcErrorDetails' as 'Status'
+--
+-- See also 'throwProtobufError'.
+toProtobufError :: GrpcException -> Either String (ProtobufError (Proto Any))
+toProtobufError err =
+    case grpcErrorDetails err of
+      Nothing ->
+        return ProtobufError{
+            protobufErrorCode    = grpcError err
+          , protobufErrorMessage = grpcErrorMessage err
+          , protobufErrorDetails = []
+          }
+      Just statusEnc -> do
+        status :: Proto Status <- parseStatus statusEnc
+        protobufErrorCode <- checkErrorCode (status ^. field @"code")
+        return ProtobufError{
+            protobufErrorCode
+          , protobufErrorMessage = constructErrorMessage (status ^. field @"message")
+          , protobufErrorDetails = status ^. field @"details"
+          }
+  where
+    -- The gRPC specification mandates
+    --
+    -- > If [Status-Details] contains a status code field, it MUST NOT
+    -- > contradict the Status header. The consumer MUST verify this
+    -- > requirement.
+    --
+    -- We cannot do this check without knowing the format of the status details,
+    -- so we do it here for the specific case of Protobuf. Since we cannot
+    -- distinguish between an absent field and a field with a default value in
+    -- Protobuf, we add a special case for this.
+    checkErrorCode :: Int32 -> Either String GrpcError
+    checkErrorCode statusCode
+      | statusCode == fieldDefault
+      = return $ grpcError err
+
+      | fromGrpcError (grpcError err) == fromIntegral statusCode
+      = return $ grpcError err
+
+      | otherwise
+      = throwError $ "'Status.code' does not match 'grpc-status'"
+
+    -- If the Status-Details does not contain a message (that is, the field is
+    -- at its default value), we fall back to the status message in the
+    -- exception itself (if any).
+    constructErrorMessage :: Text -> Maybe Text
+    constructErrorMessage msg =
+        if msg == fieldDefault
+          then grpcErrorMessage err
+          else Just msg
+
+-- | Variation of 'toProtobufError' for a homogenous list of details
+toProtobufErrorHom :: forall a.
+     Message a
+  => GrpcException -> Either String (ProtobufError (Proto a))
+toProtobufErrorHom = traverse aux <=< toProtobufError
+  where
+    aux :: Proto Any -> Either String (Proto a)
+    aux = first show . Any.unpack
---
+++ grapesy-common/src/Network/GRPC/Util/TLS.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | TLS utilities
+--
+-- Intended for qualified import.
+--
+-- > import Network.GRPC.Util.TLS (ServerValidation(..))
+-- > import Network.GRPC.Util.TLS qualified as Util.TLS
+module Network.GRPC.Util.TLS (
+    -- * Certificate store
+    CertificateStoreSpec(..)
+  , certStoreFromSystem
+  , certStoreFromCerts
+  , certStoreFromPath
+  , loadCertificateStore
+    -- * Configuration
+    -- ** Parameters
+  , ServerValidation(..)
+  , validationCAStore
+    -- ** Common to server and client
+  , SslKeyLog(..)
+  , keyLogger
+  ) where
+
+import Control.Exception
+import Data.Default
+import Data.X509 qualified as X509
+import Data.X509.CertificateStore qualified as X509
+import GHC.Generics (Generic)
+import System.Environment
+import System.X509 qualified as X509
+
+{-------------------------------------------------------------------------------
+  Certificate store
+-------------------------------------------------------------------------------}
+
+-- | Certificate store specification (for certificate validation)
+--
+-- This is a deep embedding, describing how to construct a certificate store.
+-- The actual construction happens in 'loadCertificateStore'.
+--
+-- There are three primitive ways to construct a 'CertificateStore':
+-- 'certStoreFromSystem', 'certStoreFromCerts', and 'certStoreFromPath'; please
+-- refer to the corresponding documentation.
+--
+-- You can also combine 'CertificateStore's through the 'Monoid' instance.
+data CertificateStoreSpec =
+    CertStoreEmpty
+  | CertStoreAppend CertificateStoreSpec CertificateStoreSpec
+  | CertStoreFromSystem
+  | CertStoreFromCerts [X509.SignedCertificate]
+  | CertStoreFromPath FilePath
+  deriving (Show)
+
+instance Semigroup CertificateStoreSpec where
+  (<>) = CertStoreAppend
+
+instance Monoid CertificateStoreSpec where
+  mempty = CertStoreEmpty
+
+-- | Use the system's certificate store
+certStoreFromSystem :: CertificateStoreSpec
+certStoreFromSystem = CertStoreFromSystem
+
+-- | Construct a certificate store with the given certificates
+certStoreFromCerts :: [X509.SignedCertificate] -> CertificateStoreSpec
+certStoreFromCerts = CertStoreFromCerts
+
+-- | Load certificate store from disk
+--
+-- The path may point to single file (multiple PEM formatted certificates
+-- concanated) or directory (one certificate per file, file names are hashes
+-- from certificate).
+certStoreFromPath :: FilePath -> CertificateStoreSpec
+certStoreFromPath = CertStoreFromPath
+
+-- | Load the certificate store
+loadCertificateStore :: CertificateStoreSpec -> IO X509.CertificateStore
+loadCertificateStore = go
+  where
+    go :: CertificateStoreSpec -> IO X509.CertificateStore
+    go  CertStoreEmpty         = return mempty
+    go (CertStoreAppend c1 c2) = (<>) <$> go c1 <*> go c2
+    go  CertStoreFromSystem    = X509.getSystemCertificateStore
+    go (CertStoreFromCerts cs) = return $ X509.makeCertificateStore cs
+    go (CertStoreFromPath fp)  = X509.readCertificateStore fp >>= \case
+                                   Nothing -> throwIO $ NoCertificatesAtPath fp
+                                   Just cs -> return cs
+
+data LoadCertificateStoreException =
+    NoCertificatesAtPath FilePath
+  deriving stock (Show)
+  deriving anyclass (Exception)
+
+{-------------------------------------------------------------------------------
+  Parameters
+-------------------------------------------------------------------------------}
+
+-- | How does the client want to validate the server?
+data ServerValidation =
+    -- | Validate the server
+    --
+    -- The 'CertificateStore' is a collection of trust anchors. If 'Nothing'
+    -- is specified, the system certificate store will be used.
+    ValidateServer CertificateStoreSpec
+
+    -- | Skip server validation
+    --
+    -- WARNING: This is dangerous. Although communication with the server will
+    -- still be encrypted, you cannot be sure that the server is who they claim
+    -- to be.
+  | NoServerValidation
+  deriving (Show)
+
+validationCAStore :: ServerValidation -> IO X509.CertificateStore
+validationCAStore (ValidateServer storeSpec) = loadCertificateStore storeSpec
+validationCAStore NoServerValidation         = return mempty
+
+{-------------------------------------------------------------------------------
+  Configuration common to server and client
+-------------------------------------------------------------------------------}
+
+-- | SSL key log file
+--
+-- An SSL key log file can be used by tools such as Wireshark to decode TLS
+-- network traffic. It is used for debugging only.
+data SslKeyLog =
+    -- | Don't use a key log file
+    SslKeyLogNone
+
+    -- | Use the specified path
+  | SslKeyLogPath FilePath
+
+    -- | Use the @SSLKEYLOGFILE@ environment variable to determine the key log
+    --
+    -- This is the default.
+  | SslKeyLogFromEnv
+  deriving stock (Show, Eq, Generic)
+
+instance Default SslKeyLog where
+  def = SslKeyLogFromEnv
+
+keyLogger :: SslKeyLog -> IO (String -> IO ())
+keyLogger sslKeyLog = do
+    keyLogFile <- case sslKeyLog of
+                    SslKeyLogNone    -> return $ Nothing
+                    SslKeyLogPath fp -> return $ Just fp
+                    SslKeyLogFromEnv -> lookupEnv "SSLKEYLOGFILE"
+    return $
+      case keyLogFile of
+        Nothing -> \_   -> return ()
+        Just fp -> \str -> appendFile fp (str ++ "\n")
