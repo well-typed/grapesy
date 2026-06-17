5a4f0160c07898bbd9797dae398ff22a39f61efe
Oleg Grenrus <oleg.grenrus@iki.fi>
2026-06-17 22:44:07 +0300

    Use ediffEq instead of assertEqual
    We get nicer report when things don't match.
    Using nullary property is a bit weird, but it does work as assertEqual
    substitute.

--- grapesy/test-grapesy/Test/Common/Exception.hs
+++ grapesy/test-grapesy/Test/Common/Exception.hs
@@ -2,18 +2,21 @@ module Test.Common.Exception (tests) where
 
 import Control.Exception (SomeException (..))
 import Test.Tasty
-import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 import System.ThreadManager qualified as ThreadManager
+import Data.TreeDiff.QuickCheck (ediffEq)
 
 import Network.GRPC.Common.Exception
 
 tests :: TestTree
 tests = testGroup "Test.Common.Exception"
-    [ testCase "thread-manager" $ do
+    [ testProperty "thread-manager-1" $ do
+        let expected = unlines
+                [ "KilledByThreadManager"
+                , "  IOException"
+                , "    user error (foo)"
+                ]
+
         let ex = ThreadManager.KilledByThreadManager $ Just $ SomeException $ userError "foo"
-        renderException ex @?= unlines
-            [ "KilledByThreadManager"
-            , "  IOException"
-            , "    user error (foo)"
-            ]
+        ediffEq expected (renderException ex)
     ]
